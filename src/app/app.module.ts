29f24490e3d80032c09efcfc2af6a820a95f3af4
Loïc ROY <loic.roy35@orange.fr>
2021-07-27 16:54:21 +0200

    pipe ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AvisComponent } from './avis/avis.component';
 import { CollegueComponent } from './collegue/collegue.component';
 import { ListColleguesComponent } from './list-collegues/list-collegues.component';
+import { ScorePipe } from './pipes/score.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     AvisComponent,
     CollegueComponent,
-    ListColleguesComponent
+    ListColleguesComponent,
+    ScorePipe
   ],
   imports: [
     BrowserModule,
